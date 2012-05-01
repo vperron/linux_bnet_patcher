542093d0c6da8b5515b537704fe9e1083ab4e0ff
Victor Perron <victor.perron@iso3103.net>
2012-05-02 02:47:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+linux\_bnet\_patcher
+====================
+
+
+Small executable to in-memory patch Diablo 3.
+Run Diablo 3 with wine, use 
+
+> ps aux
+
+to retrieve its processID, and then 
+
+> make linux_bnet_patcher
+> sudo ./linux_bnet_patcher <PID>
+
+That's it.
---
+++ linux_bnet_patcher.c
@@ -0,0 +1,53 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  linux_bnet_patcher.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/29/2012 01:05:28 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Victor Perron (), public@iso3103.net
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <sys/ptrace.h>
+
+#define ADDR 0x3C9C5605
+
+int main(int argc, char** argv) {
+
+	long val, patched;
+	int pid;
+
+	if (argc < 2) {
+		printf("Usage : %s <PID>\n", argv[0]);
+		return 1;
+	}
+
+	pid = atoi(argv[1]);
+
+	val = ptrace(PTRACE_ATTACH, pid, 0, 0);
+	if (val != -1) {
+		printf("Successfully attached to process: %d\n",pid);
+		val = ptrace(PTRACE_PEEKDATA, pid, ADDR, 0);
+		if (val != -1) {
+			// The read byte should be 0x75 (JNZ / JumpNotZero)
+			printf("Read byte at %x : %x\n", ADDR, (int)(val & 0xff));
+			// Replace it with 0xEB (JMP, forced jump)
+			patched = (val & 0xffffff00) | 0xeb;
+			ptrace(PTRACE_POKEDATA, pid, ADDR, (int)patched);
+			val = ptrace(PTRACE_PEEKDATA, pid, ADDR, 0);
+			printf("Read patched byte at %x : %x\n", ADDR, (int)(val & 0xff));
+			ptrace(PTRACE_DETACH, pid, 0, 0);
+			return 0;
+		}
+	}
+	perror(NULL);
+	return 1;
+}
